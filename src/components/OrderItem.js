91cb8b4d060065eadb8e4c6512cd30a53f9985c0
David J. Sanderson <sanderdj90@gmail.com>
2020-04-11 13:08:07 -0600

    feat(totalDisplay): calculate total w taxes

--- src/components/OrderItem.js
+++ src/components/OrderItem.js
@@ -4,7 +4,7 @@ const OrderItem = ({ item }) => {
   return (
     <div className="text-lg lead flex justify-between">
       <div className="ml-5">{`${item[0]}`}</div>{" "}
-      <div className="mr-10 font-bold">{`$${item[1]}`}</div>
+      <div className="mr-10">{`$${item[1]}`}</div>
     </div>
   );
 };
--- src/components/TotalDisplay.js
+++ src/components/TotalDisplay.js
@@ -3,9 +3,12 @@ import OrderItem from "./OrderItem";
 
 const TotalDisplay = ({ items }) => {
   let orderItems = items.map((item, i) => <OrderItem key={i} item={item}/>);
+  let total = items.reduce((acc, cv) => acc + cv[1], 0)
+  const calcTaxes = (total, taxRate) => ((total * taxRate) + total).toFixed(2);
   return (
     <div className="mx-auto h-32 border-2 border-black bg-indigo-300 overflow-y-scroll">
       {orderItems}
+      <div className="text-right text-lg lead mr-10 font-bold">Total: ${calcTaxes(total, .07875)}</div>
     </div>
   );
 };
